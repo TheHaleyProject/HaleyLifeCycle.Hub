7f37ff3cb9671236f2ee33fa6fc4b79bba190e79
LingamHPZ4 <rmsmech@gmail.com>
2025-09-27 19:02:29 +0400

    Base entry done.

--- HaleyWorkFlowAPI/Controllers/WorkflowVersionBaseController.cs
+++ HaleyWorkFlowAPI/Controllers/WorkflowVersionBaseController.cs
@@ -12,6 +12,19 @@ namespace Haley.Models {
            return (await _wfRepo.CreateVersionAsync(workflow_id, wf_defition?.ToString())).AsFeedBack();
         }
 
+        [HttpGet]
+        public async Task<IFeedback> GetVersion([FromQuery] int workflow_id) {
+            return (await _wfRepo.GetVersionAsync(workflow_id));
+        }
+
+        [HttpGet]
+        [Route("guid")]
+        public async Task<IFeedback> GetVersionByGuid([FromQuery] string guid) {
+            Guid input = Guid.Empty;
+            if (string.IsNullOrWhiteSpace(guid) || !(guid.IsValidGuid(out input) || guid.IsCompactGuid(out input))) return new Feedback(false, "Input is not a valid guid");
+            return (await _wfRepo.GetVersionByGUIDAsync(input));
+        }
+
         public WorkflowVersionBaseController(IWorkflowRepository wfRepository, IWorkflowEngine wfEngine, ILoggerProvider logprovider) : base(wfRepository,wfEngine, logprovider.CreateLogger(nameof(WorkflowVersionBaseController))) {
            
         }
